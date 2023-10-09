b6ab9c367f11ef123f4e353962677327fec75a0b
Bonevy BEBY <bonevybeby@gmail.com>
2023-10-09 13:09:05 +0200

    Change-Id: I0000000000000000000000000000000000000000
    Change-Id: I0c8ff92f4383ff1f4e92ddd092d2454b029c2884

---
+++ siacdas/src/main/java/com/formationkilo/siacdas/SiacdasApplication.java
@@ -0,0 +1,13 @@
+package com.formationkilo.siacdas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiacdasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiacdasApplication.class, args);
+	}
+
+}
---
+++ siacdas/src/test/java/com/formationkilo/siacdas/SiacdasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formationkilo.siacdas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiacdasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
