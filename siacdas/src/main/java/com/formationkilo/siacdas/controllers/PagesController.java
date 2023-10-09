3e7354f1ee6ed9b72fb21d9c4dc6165c931d8a73
Bonevy BEBY <bonevybeby@gmail.com>
2023-10-09 14:57:23 +0200

    Manage a query string with a java EE method: public String home(HttpServletRequest request, ModelMap modelMap) and thymeleaf

--- siacdas/src/main/java/com/formationkilo/siacdas/controllers/PagesController.java
+++ siacdas/src/main/java/com/formationkilo/siacdas/controllers/PagesController.java
@@ -1,15 +1,24 @@
 package com.formationkilo.siacdas.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class PagesController {
 
 	@GetMapping("/")
 	//@ResponseBody
-	public String home() {
-		return "pages/home";
+	public String home(HttpServletRequest request, ModelMap modelMap) {
+		String param=request.getParameter("name");
+		String name=param!=null && 
+				!param.isEmpty()
+				?param:"Renseignez votre nom pour voir.";
+		        modelMap.put("name", name);
+		 return "pages/home";
+			
+		
 	}
 }
